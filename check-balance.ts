c49a986990a4c29d402f680c4289cc7f1c5ebbf3
Kausik Pattanaik <kausikpattanaik2002@gmail.com>
2025-02-03 18:51:51 +0530

    money transfer

--- check-balance.ts
+++ check-balance.ts
@@ -4,13 +4,12 @@ const suppliedPublicKey = process.argv[2];
 if (!suppliedPublicKey) {
   throw new Error("Provide a public key to check the balance of!");
 }
- 
 const connection = new Connection("https://api.devnet.solana.com", "confirmed");
  
 const publicKey = new PublicKey(suppliedPublicKey);
  
 const balanceInLamports = await connection.getBalance(publicKey);
- 
+  
 const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
  
 console.log(
